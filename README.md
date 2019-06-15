df71eaf2689c5b59af7cac58a20afc00784721f5
Connor80 <connor@connortroy.com>
2019-06-15 12:43:40 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MLB_GameoftheDay
+Sends a text at noon when the Braves are the Free Game of The Day on MLB.TV.
---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request as REQUEST, redirect
+from twilio.rest import Client
+from apscheduler.schedulers.background import BackgroundScheduler
+from bs4 import BeautifulSoup
+import requests
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    schedule = BackgroundScheduler(daemon=True)
+    schedule.add_job(daily_update, 'cron', hour='*/12')
+    schedule.start()
+
+    text = "Checking for free game..."
+
+    return text
+
+def daily_update():
+    account_sid = app.config['ACCOUNTSID']
+    auth_token  = app.config['AUTHTOKEN']
+    client = Client(account_sid, auth_token)
+
+    url = 'https://www.mlb.com/live-stream-games'
+    webpage = requests.get(url)
+    soup = BeautifulSoup(webpage.text, 'html.parser')
+
+    free_game = soup.findAll("span", {"class":"free-game-txt"})
+    teams = free_game[0]
+    for game in soup.findAll("tr", {"class":"free-game"}):
+        result = game.get_text()
+    matchup = " ".join(result.split())
+
+    if "Braves" in matchup:
+        message = client.messages \
+                        .create(
+                             body=" ".join(result.split()),
+                             from_=app.config['FROM'],
+                             to=app.config['TO']
+                         )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
